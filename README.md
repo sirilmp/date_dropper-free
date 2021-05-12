6ed3f2d1926a84b04e0e5cf5900c05c6f486da23
sirilmp <sirilmp8@gmail.com>
2021-05-12 13:21:34 +0530

    datepicker

---
+++ README.md
@@ -0,0 +1,2 @@
+its totally free version for date dropper use same code and alo you can change color etc
+contact : www.sirilmp.online 
---
+++ my-js.js
@@ -0,0 +1,5 @@
+$('.date').dateDropper();
+      
+// change the placeholder
+var format="DD/MM/YYYY";
+document.getElementById('date').value=format
